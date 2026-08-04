74e33621544c685e5d7a8d80bbed05f861375959
aetherord <aetherord@outlook.com>
2026-08-04 22:58:15 +0300

    Fixed UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const playfair = Playfair_Display({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Aether",
